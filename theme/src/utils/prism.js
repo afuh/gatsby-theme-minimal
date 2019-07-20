4facc749ff2529bf5dd361f67bf425a52fa2966d
Axel <axelfuh@gmail.com>
2019-07-20 12:12:30 +0200

    Update prismjs theme

--- theme/src/utils/prism.js
+++ theme/src/utils/prism.js
@@ -1,109 +1,129 @@
 import { css } from "styled-components"
 
+const schema = {
+  gray: "#9F9FA3",
+  cyan: '#00BCD4',
+  purple: "#c678dd",
+  blue: '#61afef',
+  yellow: '#e5c07b',
+  brown: '#d19a66',
+  green: '#98c379',
+  inline: {
+    back: '#eaeced',
+    color: '#323232'
+  }
+}
+
 export const prismCSS = css`
-/**
- * Dracula Theme originally by Zeno Rocha [@zenorocha]
- * https://draculatheme.com/
- *
- * Ported for PrismJS by Albert Vallverdu [@byverdu]
- */
+  code {
+    font-family: Menlo,Monaco,Consolas,"Courier New",monospace;
+    line-height: 1.4rem
+  }
 
-code[class*="language-"],
-pre[class*="language-"] {
-  color: #f8f8f2;
-  background: none;
-  text-shadow: 0 1px rgba(0, 0, 0, 0.3);
-  font-family: Consolas, Monaco, 'Andale Mono', 'Ubuntu Mono', monospace;
-  text-align: left;
-  white-space: pre;
-  word-spacing: normal;
-  word-break: normal;
-  word-wrap: normal;
-  line-height: 1.5;
-  -moz-tab-size: 4;
-  -o-tab-size: 4;
-  tab-size: 4;
-  -webkit-hyphens: none;
-  -moz-hyphens: none;
-  -ms-hyphens: none;
-  hyphens: none; }
+  code[class*="language-"],
+  pre[class*="language-"] {
+  	color: ${({ theme }) => theme.white};
+  	background: none;
+  	text-align: left;
+  	white-space: pre;
+  	word-spacing: normal;
+  	word-break: normal;
+  	word-wrap: normal;
+  	line-height: 1.5;
+  	tab-size: 4;
+  	hyphens: none;
+  }
 
-/* Code blocks */
-pre[class*="language-"] {
-  padding: 1em;
-  margin: .5em 0;
-  overflow: auto;
-  border-radius: 0.3em; }
+  /* Code blocks */
+  pre[class*="language-"] {
+    background: ${({ theme }) => theme.black};
+  	padding: 1em;
+  	margin: .5em 0;
+  	overflow: auto;
+  	border-radius: 2px;
+    box-shadow: 0 2px 2px 0 rgba(0,0,0,0.16), 0 0 0 1px rgba(0,0,0,0.08)
+  }
 
-:not(pre) > code[class*="language-"],
-pre[class*="language-"] {
-  background: #282a36; }
+  /* Inline code */
+  p > code[class*="language-"],
+  li > code[class*="language-"] {
+    background: ${schema.inline.back};
+    color: ${schema.inline.color};
+    padding: 0.2em 0.4em;
+    border-radius: 0;
+    white-space: normal;
+  }
 
-/* Inline code */
-:not(pre) > code[class*="language-"] {
-  padding: .1em;
-  border-radius: .3em;
-  white-space: normal; }
+  .token.comment,
+  .token.prolog,
+  .token.doctype,
+  .token.cdata {
+  	color: slategray;
+  }
 
-.token.comment,
-.token.prolog,
-.token.doctype,
-.token.cdata {
-  color: #6272a4; }
+  .namespace {
+  	opacity: .7;
+  }
 
-.token.punctuation {
-  color: #6c6783; }
+  .token.property,
+  .token.tag,
+  .token.constant,
+  .token.symbol,
+  .token.deleted {
+    color: ${schema.yellow};
+  }
 
-.namespace {
-  opacity: .7; }
+  .token.boolean,
+  .token.number {
+  	color: ${schema.cyan};
+  }
 
-.token.property,
-.token.tag,
-.token.constant,
-.token.symbol,
-.token.deleted {
-  color: #ff79c6; }
+  .token.selector,
+  .token.string,
+  .token.char,
+  .token.builtin,
+  .token.inserted {
+  	color: ${schema.green};
+  }
 
-.token.boolean,
-.token.number {
-  color: #bd93f9; }
+  .token.attr-name {
+    color: ${schema.brown};
+  }
 
-.token.selector,
-.token.attr-name,
-.token.string,
-.token.char,
-.token.builtin,
-.token.inserted {
-  color: #50fa7b; }
+  .token.operator,
+  .token.punctuation,
+  .token.entity,
+  .token.url,
+  .language-css .token.string,
+  .style .token.string,
+  .token.variable {
+  	color: ${schema.gray};
+  }
 
-.token.operator,
-.token.entity,
-.token.url,
-.language-css .token.string,
-.style .token.string,
-.token.variable {
-  color: #f8f8f2; }
+  .token.atrule,
+  .token.attr-value,
+  .token.function {
+  	color: ${schema.blue};
+  }
 
-.token.atrule,
-.token.attr-value,
-.token.function,
-.token.class-name {
-  color: #f1fa8c; }
+  .token.keyword {
+  	color: ${schema.purple};
+  }
 
-.token.keyword {
-  color: #8be9fd; }
+  .token.regex,
+  .token.important {
+  	color: #fd971f;
+  }
 
-.token.regex,
-.token.important {
-  color: #ffb86c; }
+  .token.important,
+  .token.bold {
+  	font-weight: bold;
+  }
+  .token.italic {
+  	font-style: italic;
+  }
 
-.token.important,
-.token.bold {
-  font-weight: bold; }
-
-.token.italic {
-  font-style: italic; }
-
-.token.entity {
-  cursor: help; }
+  .token.entity {
+  	cursor: help;
+  }
 `
